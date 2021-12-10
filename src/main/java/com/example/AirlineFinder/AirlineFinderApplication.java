e7ac6be6b2bdc5314d0343f33b355e87fc960c19
bentagtow <94013701+bentagtow@users.noreply.github.com>
2021-12-10 17:32:34 -0600

    first commit

---
+++ src/main/java/com/example/AirlineFinder/AirlineFinderApplication.java
@@ -0,0 +1,13 @@
+package com.example.AirlineFinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlineFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AirlineFinder/AirlineFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AirlineFinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
